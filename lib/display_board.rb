d46aa95d5022efe1006f842fb7f6dad100f3fdaf
Tim Quimbo <tquimbo1@gmail.com>
2021-02-26 14:13:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
+<<<<<<< HEAD
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+puts "   |   |   "
+# "-----------", "   |   |   ", "-----------", "   |   |   "
+>>>>>>> f7011a3c3102c5f0a6572ebd7d6a30dcfe63b442
 end
